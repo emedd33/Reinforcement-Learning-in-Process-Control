55fdced5e5b711b5b23f3efd80654e2e48904c86
Eskild <eskild.emedd33@gmail.com>
2019-01-10 15:37:52 +0100

    initial commit

--- models/Agent.py
+++ models/Agent.py
@@ -4,33 +4,43 @@ from params import *
 import numpy as np 
 class Agent():
     def __init__(self,
-            hidden_size=[10,10],
-            input_size=OBSERVATIONS*2-1, #All states plus the gradients
-            output_size=VALVE_POSITIONS
+            hl_size=[10,10],
+            state_size=OBSERVATIONS*2-1, #All states plus the gradients
+            action_size=VALVE_POSITIONS
         ):
+
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+
         self.action_memory = deque(maxlen=3000) 
         self.state_memory = deque(maxlen=3000) 
         
         self.get_valve_positions(output_size)
-        self.build_ANN(input_size,hidden_size,output_size,learning_rate=0.01)
+        self.build_ANN(state_size,hl_size,action_size,learning_rate=0.01)
     
-    def build_ANN(self,input_size,hidden_size,output_size,learning_rate):    
+    def build_ANN(self,state_size,hl_size,action_size,learning_rate):    
         # Defining network model
         self.model = keras.Sequential()
-        self.model.add(keras.layers.Dense(hidden_size[0],input_shape=(input_size,),activation='relu'))
-        for layer in hidden_size:
-            self.model.add(keras.layers.Dense(layer,activation='relu'))
-        self.model.add(keras.layers.Dense(output_size,activation='softmax'))
+        self.model.add(keras.layers.Dense(hl_size[0],input_shape=(state_size,),activation='relu'))
+        for i in hl_size:
+            self.model.add(keras.layers.Dense(i,activation='relu'))
+        self.model.add(keras.layers.Dense(action_size,activation='softmax'))
         
         self.model.compile(
             loss='mse',
             optimizer=keras.optimizers.Adam(lr=learning_rate)
             )
     
-    def get_valve_positions(self,output_size):
+    def get_valve_positions(self,action_size):
         valve_positions= []
-        for i in range(output_size):
-            valve_positions.append(i/(output_size-1))
+        for i in range(action_size):
+            valve_positions.append(i/(action_size-1))
         self.valve_positions = np.array(valve_positions)
 
     def predict(self,x):
