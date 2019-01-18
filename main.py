45592f3b1672d7eaafdd013d277e0d370992035d
Eskild <eskild.emedd33@gmail.com>
2019-01-18 10:44:27 +0100

    Add keyboard interuption and print function for overview of simulation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+print("#### IMPORTING ####")
 from models.environment import Environment
 from models.Agent import Agent
 from params import * # Parameters used in main
@@ -13,10 +14,8 @@ def main():
     environment = Environment()
     agent = Agent()
     # ================= Running episodes =================#
-    running=True
     all_rewards = [] 
     batch_size = BATCH_SIZE
-    reward_index = int(0)
     for e in range(EPISODES):
         state, action, next_state,episode_reward = environment.reset() # Reset level in tank
         # Running through states in the episode
@@ -33,23 +32,28 @@ def main():
                 environment.render(z,next_state[-1])
             if len(agent.memory)>batch_size:
                 agent.replay(batch_size)
-        # Live plot rewards
-        agent.decay_exploration()
-        all_rewards.append(episode_reward)
-        if LIVE_REWARD_PLOT:
             if keyboard.is_pressed('ctrl+c'):
                 break
-            else:
-                environment.plot(all_rewards,agent.epsilon)       
+        # Live plot rewards
+        # agent.decay_exploration()
+        all_rewards.append(episode_reward)
+        if keyboard.is_pressed('ctrl+c'):
+                break
+        if LIVE_REWARD_PLOT:
+            environment.plot(all_rewards,agent.epsilon)       
         if not environment.running:
             break
                     
         
         
-    print("##### {} EPISODES DONE #####".format(EPISODES))
-    print("Mean rewards for all episodes: {}".format(np.mean(all_rewards))) 
+    print("##### {} EPISODES DONE #####".format(e))
+    print("Max rewards for all episodes: {}".format(np.max(all_rewards))) 
     print("Mean rewards for the last 10 episodes: {}".format(np.mean(all_rewards[-10:]))) 
     if SAVE_ANN_MODEL:
         print("ANN_Model was saved")
 if __name__ == "__main__":
+    print("#### SIMULATION STARTED ####")
+    print("  Max number of episodes: {}".format(EPISODES))
+    print("  Max time in each episode: {}".format(MAX_TIME))
+    print("  {}Rendring simulation ".format("" if RENDER else "Not "))
     main()
