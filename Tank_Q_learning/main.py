05ed2e312285742975399b49ee8246be1a50e6a4
Eskild <eskild.emedd33@gmail.com>
2019-02-09 20:05:12 +0100

    Fix minor bug with appending reward to all_reward

--- Tank_Q_learning/main.py
+++ Tank_Q_learning/main.py
@@ -11,17 +11,19 @@ import keyboard
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 def main():
-    #============= Initialize variables ===========#
+    #============= Initialize variables and objects ===========#
     max_mean_reward = 0
     environment = Environment()
     agent = Agent()
-    # ================= Running episodes =================#
+    
     all_rewards = [] 
     all_mean_rewards = []
     batch_size = BATCH_SIZE
+    
+    # ================= Running episodes =================#
     for e in range(EPISODES):
         state, next_state,episode_reward = environment.reset() # Reset level in tank
-        # Running through states in the episode
+        
         for _ in range(MAX_TIME):    
             action = agent.act(state) # get action choice from state
             z = agent.action_choices[action] # convert action choice into valve position
@@ -38,6 +40,9 @@ def main():
                 environment.render(z)
             if terminated:
                 break 
+            # End for
+        all_rewards.append(np.sum(np.array(episode_reward)))
+        
         # Print mean reward and save better models
         if e % MEAN_EPISODE == 0 and e != 0:
             mean_reward = np.mean(all_rewards[-MEAN_EPISODE:])
@@ -49,15 +54,10 @@ def main():
         # Train model
         if (agent.is_ready(batch_size)):
             agent.Qreplay(batch_size)
-        if keyboard.is_pressed('ctrl+x'):
-            break
-        
-        # Live plot rewards
-        all_rewards.append(np.sum(np.array(episode_reward)))
-        
         
         if keyboard.is_pressed('ctrl+c'):
-                break
+            break
+        
         if LIVE_REWARD_PLOT:
             environment.plot(all_rewards,agent.epsilon)       
         if not environment.running:
