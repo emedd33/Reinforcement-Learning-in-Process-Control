ff1d0069c2f745cd6638438ca8b582a515c771ab
Eskild <eskild.emedd33@gmail.com>
2019-03-11 10:57:53 +0100

    refactor main.py in 2 Tanks

--- Q_learning/Tank_2/main.py
+++ Q_learning/Tank_2/main.py
@@ -1,123 +1,110 @@
-print("#### IMPORTING ####")
-
+# Add the ptdraft folder path to the sys.path list
+# sys.path.append("C:/Users/eskil/Google Drive/Skolearbeid/5. klasse/Master")
 from models.environment import Environment
 from models.Agent import Agent
-from params import (
-    BATCH_SIZE,
-    EPISODES,
-    MAX_TIME,
-    TBCC,
-    MEAN_EPISODE,
-    LIVE_REWARD_PLOT,
-    SAVE_ANN_MODEL,
-    RENDER,
-    NUMBER_OF_HIDDEN_LAYERS,
-    N_TANKS,
-)
+from params import MAIN_PARAMS, AGENT_PARAMS, TANK_PARAMS, TANK_DIST
 import os
 import matplotlib.pyplot as plt
-
-plt.style.use("ggplot")
 import numpy as np
 import keyboard
+from rewards import get_reward_2 as get_reward
+
+plt.style.use("ggplot")
+
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 
 def main():
-    # ============= Initialize variables ===========#
-    max_mean_reward = 0
-    environment = Environment()
-    agent = Agent()
-    # ================= Running episodes =================#
+    # ============= Initialize variables and objects ===========#
+    max_mean_reward = 49
+    environment = Environment(TANK_PARAMS, TANK_DIST, MAIN_PARAMS)
+    agent = Agent(AGENT_PARAMS)
+    mean_episode = MAIN_PARAMS["MEAN_EPISODE"]
+    episodes = MAIN_PARAMS["EPISODES"]
     all_rewards = []
     all_mean_rewards = []
-    batch_size = BATCH_SIZE
-    for e in range(EPISODES):
-        state, action_delay, next_state, episode_reward = (
-            environment.reset()
-        )  # Reset level in tank
-        # Running through states in the episode
-        for _ in range(MAX_TIME):
-            if action_delay >= TBCC:
-                action = agent.act(state)
-                z = agent.action_choices[action]
-                action_delay = 0
-            else:
-                action_delay += 1
-            terminated, next_state = environment.get_next_state(z, state)
-            reward = environment.get_reward(next_state, terminated)
-            episode_reward.append(np.sum(reward))
-            if action_delay >= TBCC or terminated:
-                agent.remember(state, action, next_state, reward, terminated)
-            state = next_state
-            if True in terminated:
-                break
-            if environment.show_rendering:
-                environment.render(z)
-        if e % MEAN_EPISODE == 0 and e != 0:
-            mean_reward = np.mean(all_rewards[-MEAN_EPISODE:])
-            all_mean_rewards.append(mean_reward)
-            print(
-                "Mean rewards for the last {} of {}/{} episodes : {} explore: {}".format(
-                    MEAN_EPISODE,
-                    e,
-                    EPISODES,
-                    mean_reward,
-                    round(agent.epsilon, 2),
+
+    # ================= Running episodes =================#
+    try:
+        for e in range(episodes):
+            states, episode_reward = environment.reset()  # Reset level in tank
+            for t in range(MAIN_PARAMS["MAX_TIME"]):
+                action = agent.act(states[-1])  # get action choice from state
+                z = agent.get_z(action)
+
+                terminated, next_state = environment.get_next_state(
+                    z, states[-1], t
+                )  # Calculate next state with action
+                reward = get_reward(
+                    next_state, terminated
+                )  # get reward from transition to next state
+
+                # Store data
+                episode_reward.append(np.sum(reward))
+
+                states.append(next_state)
+                agent.remember(states, reward, terminated, t)
+
+                if environment.show_rendering:
+                    environment.render(z)
+                if terminated:
+                    break
+
+            all_rewards.append(np.sum(np.array(episode_reward)))
+
+            # Print mean reward and save better models
+            if e % mean_episode == 0 and e != 0:
+                mean_reward = np.mean(all_rewards[-mean_episode:])
+                all_mean_rewards.append(mean_reward)
+                print(
+                    "{} of {}/{} episodes\
+                     reward: {} explore: {}".format(
+                        mean_episode,
+                        e,
+                        episodes,
+                        round(mean_reward, 2),
+                        round(agent.epsilon, 2),
+                    )
                 )
-            )
-            if SAVE_ANN_MODEL:
-                if mean_reward > max_mean_reward:
-                    for i, model in enumerate(agent.ANN_models):
-                        model_name = (
-                            "/ANN_"
-                            + str(NUMBER_OF_HIDDEN_LAYERS)
-                            + "HL_"
-                            + str(i)
-                        )
-                        model_path = (
-                            "Tank_Q_learning_2/models/saved_models"
-                            + model_name
-                            + ".h5"
-                        )
-                        model.save(model_path)
-                    print("ANN_Model was saved")
-                    max_mean_reward = mean_reward
-        if agent.is_ready(batch_size):
-            loss = agent.Qreplay(batch_size)
-        if keyboard.is_pressed("ctrl+x"):
-            break
+                if agent.save_model_bool:
+                    max_mean_reward = agent.save_model(
+                        mean_reward, max_mean_reward
+                    )
+                # Train model
+            if agent.is_ready():
+                agent.Qreplay(e)
 
-        # Live plot rewards
-        all_rewards.append(np.sum(np.array(episode_reward)))
+            if keyboard.is_pressed("ctrl+x"):
+                break
 
-        if keyboard.is_pressed("ctrl+x"):
-            break
-        if LIVE_REWARD_PLOT:
-            environment.plot(all_rewards, agent.epsilon)
-        if not environment.running:
-            break
-
+            if environment.live_plot:
+                environment.plot(all_rewards, agent.epsilon)
+            if not environment.running:
+                break
+            if agent.epsilon <= agent.epsilon_min:
+                print(len(agent.memory))
+                break
+    except KeyboardInterrupt:
+        pass
+    print("Memory length: {}".format(len(agent.memory)))
     print("##### {} EPISODES DONE #####".format(e + 1))
     print("Max rewards for all episodes: {}".format(np.max(all_rewards)))
-    print(
-        "Mean rewards for the last 10 episodes: {}".format(
-            np.mean(all_rewards[-10:])
-        )
-    )
     plt.ioff()
     plt.clf()
-    plt.plot(all_mean_rewards)
-    plt.ylabel("Episodic reward")
-    plt.xlabel("Episode")
+    x_range = np.arange(0, e - e % mean_episode, mean_episode)
+    plt.plot(x_range, all_mean_rewards)
+    plt.ylabel("Mean rewards of last {} episodes".format(mean_episode))
     plt.show()
 
 
 if __name__ == "__main__":
     print("#### SIMULATION STARTED ####")
-    print("  Max number of episodes: {}".format(EPISODES))
-    print("  Max time in each episode: {}".format(MAX_TIME))
-    print("  Max reward in each episode: {}".format(MAX_TIME * N_TANKS))
-    print("  {}Rendring simulation ".format("" if RENDER else "Not "))
+    print("  Max number of episodes: {}".format(MAIN_PARAMS["EPISODES"]))
+    print("  Max time in each episode: {}".format(MAIN_PARAMS["MAX_TIME"]))
+    print(
+        "  {}Rendring simulation ".format(
+            "" if MAIN_PARAMS["RENDER"] else "Not "
+        )
+    )
     main()
