fixed policy gradient

Policy_Gradient/Tank_1/evalv_controller.py
@@ -28,9 +28,7 @@ def main():
     h.append(h_)
     for t in range(MAIN_PARAMS["MAX_TIME"]):
         action = agent.act(state[-1])  # get action choice from state
-        z_ = agent.action_choices[
-            action
-        ]  # convert action choice into valve position
+        z_ = agent.get_valve_position(action)
         z.append(np.array(z_))
         terminated, next_state = environment.get_next_state(
             z[-1], state[-1], t
@@ -60,7 +58,6 @@ def main():
 
         if not environment.running:
             break
-    print(np.sum(episode_reward))
 
     _, (ax1, ax2, ax3) = plt.subplots(3, sharex=False, sharey=False)
     d = np.array(d)

Policy_Gradient/Tank_1/main.py
@@ -31,8 +31,8 @@ def main():
         for e in range(episodes):
             states, episode_reward = environment.reset()  # Reset level in tank
             for t in range(MAIN_PARAMS["MAX_TIME"]):
-                z = agent.act(states[-1])  # get action choice from state
-
+                actions = agent.act(states[-1])  # get action choice from state
+                z = agent.get_valve_position(actions)
                 terminated, next_state = environment.get_next_state(
                     z, states[-1], t
                 )  # Calculate next state with action
@@ -71,9 +71,8 @@ def main():
                     max_mean_reward = agent.save_model(
                         mean_reward, max_mean_reward
                     )
-                # Train model
             if agent.is_ready():
-                agent.Qreplay(e)
+                agent.PolicyGradientReplay(e)
 
             if keyboard.is_pressed("ctrl+x"):
                 break
@@ -91,8 +90,9 @@ def main():
     print("Max rewards for all episodes: {}".format(np.max(all_rewards)))
     plt.ioff()
     plt.clf()
-    x_range = np.arange(1, e - e % mean_episode, mean_episode)
+    x_range = np.arange(0, e - e % mean_episode, mean_episode)
     plt.plot(x_range, all_mean_rewards)
+    plt.ylim([0, 200])
     plt.ylabel("Mean rewards of last {} episodes".format(mean_episode))
     plt.show()
 

Policy_Gradient/Tank_1/models/Network.py
@@ -1,25 +1,28 @@
 import torch
 import torch.nn as nn
+from torch.distributions import Bernoulli
+import numpy as np
 
 # from IPython.core.debugger import set_trace
 
 
 class Net(nn.Module):
-    def __init__(self, input_size, hidden_size, action_size, learning_rate):
+    def __init__(self, input_size, hidden_size, learning_rate, action_size):
         super(Net, self).__init__()
+        self.action_size = action_size
         self.n_hl = len(hidden_size)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Softmax()
         if self.n_hl == 0:  # No hidden layer
             self.input = nn.Linear(input_size, action_size)
             self.hl1 = None
             self.hl2 = None
         elif self.n_hl == 1:  # One hidden layer
             self.input = nn.Linear(input_size, hidden_size[0])
-            self.relu = nn.ReLU()
             self.hl1 = nn.Linear(hidden_size[0], action_size)
             self.hl2 = None
         elif self.n_hl == 2:  # Two hidden layers
             self.input = nn.Linear(input_size, hidden_size[0])
-            self.relu = nn.ReLU()
             self.hl1 = nn.Linear(hidden_size[0], hidden_size[1])
             self.hl2 = nn.Linear(hidden_size[1], action_size)
         else:
@@ -47,4 +50,21 @@ class Net(nn.Module):
             out = self.hl1(out)
             out = self.relu(out)
             out = self.hl2(out)
+        out = self.sigmoid(out)
         return out
+
+    def backward(self, states, actions, rewards, dummy_index):
+        self.optimizer.zero_grad()
+        for i in range(len(states)):
+            state = states[i]
+            # action = torch.autograd.Variable(torch.FloatTensor([actions[i]]))
+            reward = rewards[i]
+
+            probs = self.forward(state)
+            m = Bernoulli(probs[actions[i]])
+            loss = (
+                -m.log_prob(actions[i]) * reward
+            )  # Negtive score function x reward
+            loss.backward()
+
+            self.optimizer.step()