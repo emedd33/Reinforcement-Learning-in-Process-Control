a137475ae4cd9195d47f658436b898acc8136d42
Eskild <eskild.emedd33@gmail.com>
2019-03-19 14:19:51 +0100

    create 6_tank policy gradient folder

---
+++ Policy_Gradient/Tank_6/params.py
@@ -0,0 +1,79 @@
+MAIN_PARAMS = {
+    "EPISODES": 3000,
+    "MEAN_EPISODE": 5,
+    "MAX_TIME": 200,
+    "RENDER": False,
+    "LIVE_REWARD_PLOT": False,
+}
+
+AGENT_PARAMS = {
+    "N_TANKS": 2,
+    "SS_POSITION": 0.5,
+    "VALVE_START_POSITION": 0.5,
+    "ACTION_DELAY": [5, 5],
+    "INIT_ACTION": 0,
+    "EPSILON_MIN": 0.05,
+    "VALVEPOS_UNCERTAINTY": 0.02,
+    "EPSILON_DECAY": [0.99, 0.992],
+    "LEARNING_RATE": 0.001,
+    "HIDDEN_LAYER_SIZE": [10],
+    "BATCH_SIZE": 5,
+    "MEMORY_LENGTH": 10000,
+    "OBSERVATIONS": 4,  # level, gradient, is_above 0.5, prevous valve position
+    "GAMMA": 0.5,
+    "EPSILON": 0.05,
+    "Z_VARIANCE": 0.05,
+    "SAVE_MODEL": True,
+    "LOAD_MODEL": False,
+    "TRAIN_MODEL": True,
+    "MODEL_NAME": "",
+}
+AGENT_PARAMS["BUFFER_THRESH"] = AGENT_PARAMS["BATCH_SIZE"] * 1
+
+# Model parameters Tank 1
+TANK1_PARAMS = {
+    "height": 10,
+    "init_level": 0.5,
+    "width": 10,
+    "pipe_radius": 0.5,
+    "max_level": 0.75,
+    "min_level": 0.25,
+}
+
+TANK1_DIST = {
+    "add": True,
+    "pre_def_dist": False,
+    "nom_flow": 1,  # 2.7503
+    "var_flow": 0.1,
+    "max_flow": 2,
+    "min_flow": 0.7,
+    "add_step": True,
+    "step_time": int(MAIN_PARAMS["MAX_TIME"] / 2),
+    "step_flow": 2,
+    "max_time": MAIN_PARAMS["MAX_TIME"],
+}
+
+TANK2_PARAMS = {
+    "height": 10,
+    "init_level": 0.5,
+    "width": 10,
+    "pipe_radius": 0.5,
+    "max_level": 0.75,
+    "min_level": 0.25,
+}
+
+TANK2_DIST = {
+    "add": False,
+    "pre_def_dist": False,
+    "nom_flow": 1,  # 2.7503
+    "var_flow": 0.1,
+    "max_flow": 1.5,
+    "min_flow": 0.7,
+    "add_step": False,
+    "step_time": int(MAIN_PARAMS["MAX_TIME"] / 2),
+    "step_flow": 2,
+    "max_time": MAIN_PARAMS["MAX_TIME"],
+}
+
+TANK_PARAMS = [TANK1_PARAMS, TANK2_PARAMS]
+TANK_DIST = [TANK1_DIST, TANK2_DIST]
---
+++ Policy_Gradient/Tank_6/rewards.py
@@ -0,0 +1,55 @@
+import numpy as np
+from params import TANK_PARAMS
+
+ss_position = TANK_PARAMS[0]["init_level"]
+
+
+def sum_rewards(states, terminated, get_reward):
+    rewards = []
+    for i in range(len(states)):
+        rewards.append(get_reward(states[i], terminated[i]))
+    return rewards
+
+
+def get_reward_1(state, terminated):
+    "Calculates the environments reward for the next state"
+
+    if terminated:
+        return -10
+    if state[0] > 0.25 and state[0] < 0.75:
+        return 1
+    return 0
+
+
+def get_reward_2(state, terminated):
+    "Calculates the environments reward for the next state"
+    if terminated:
+        return -10
+    if state[0] > 0.4 and state[0] < 0.6:
+        return 1
+    return 0
+
+
+def get_reward_3(state, terminated):
+    "Calculates the environments reward for the next state"
+    if terminated:
+        return -1
+    if state[0] > 0.4 and state[0] < 0.6:
+        return 1
+    return 0
+
+
+def get_reward_ABS(state, terminated):
+    "Calculates the environments reward for the next state"
+
+    if terminated:
+        return -10
+    return np.absolute(ss_position - state[0])
+
+
+def get_reward_SSE(state, terminated):
+    "Calculates the environments reward for the next state"
+
+    if terminated:
+        return -10
+    return (ss_position - state[0]) ** 2
